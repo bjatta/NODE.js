998e475c40a59d1493459cc934b1d829cabe9495
Victor <me@bjatta.ru>
2017-04-20 22:27:37 +0300

    added favicon.png

--- lecture 03/js/hw03.if.03.js
+++ lecture 03/js/hw03.if.03.js
@@ -4,25 +4,43 @@
 ((bankNotes,coins) => {
     let bankNotesString = '';
     let coinsString = '';
+    let offsetB = bankNotes%100;
+    let offsetC = coins%100;
 
-    switch (bankNotes%10){
-        case 1: bankNotesString = ' рубль '; break;
-        case 2:
-        case 3:
-        case 4: bankNotesString = ' рубля '; break;
-        default: bankNotesString = ' рублей ';
+    if ((offsetB < 20)&&(offsetB >5)) bankNotesString = ' рублей ';
+        else {
+        switch (bankNotes % 10) {
+            case 1:
+                bankNotesString = ' рубль ';
+                break;
+            case 2:
+            case 3:
+            case 4:
+                bankNotesString = ' рубля ';
+                break;
+            default:
+                bankNotesString = ' рублей ';
+        }
     }
-    switch (coins%10){
-        case 1: coinsString = ' копейка '; break;
-        case 2:
-        case 3:
-        case 4: coinsString = ' копейки '; break;
-        default: coinsString = ' копеeк ';
+    if ((offsetC < 20)&&(offsetC >5)) coinsString = ' копеeк ';
+        else {
+        switch (coins % 10) {
+            case 1:
+                coinsString = ' копейка ';
+                break;
+            case 2:
+            case 3:
+            case 4:
+                coinsString = ' копейки ';
+                break;
+            default:
+                coinsString = ' копеeк ';
+        }
     }
     console.log(
         ((bankNotes)?bankNotes+bankNotesString:'')
         +((coins)?coins+coinsString:''));
 })
-(Math.round(Math.random()*100),
+(Math.round(Math.random()*122),
  Math.round(Math.random()*100)
 );
