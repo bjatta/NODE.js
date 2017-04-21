de1050dc5d7c90cc22c931c37fb8f554ed2e792f
Victor <me@bjatta.ru>
2017-04-21 14:00:47 +0300

    additional task 4 done 1*** left

--- lecture 03/js/hw03.add.05.js
+++ lecture 03/js/hw03.add.05.js
@@ -1,6 +1,16 @@
 /**
  * Created by Bjatta on 20.04.2017.
  */
-(()=> {
+((n)=> {
+    var result = [];
+    console.log('task #5: ... for n: '+n)
+    for (let i = 1; i <= n; i++){
+        for (let j = i; j <= n; j++) {
+            if (0 != j%i || i == 1) {
+                result.push({up:i,down:j,value:i/j});
+            }
+        }
+    }
+    console.log(result.sort((a,b)=>(a.value - b.value)).map((el)=>el.up+'/'+el.down).join(' '));
 }
-)();
+)(Math.round(Math.random()*15)+1);
