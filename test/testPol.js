9604de9462f8e31b9b62a8c0e8d2b46b20ffd2b2
Victor <me@bjatta.ru>
2017-05-08 23:16:48 +0300

    CSS user info & user list fix

---
+++ test/testPol.js
@@ -0,0 +1,16 @@
+/**
+ * Created by Bjatta on 08.05.2017 at 14:22.
+ */
+;(() => {
+    "use strict";
+    console.log(a,i);
+    let array = [1,2,3,4,5,'sdrwer',{werwer:12},[]];
+    console.log(array.length);
+    array.length = 33;
+    var a=12;
+    console.log(a,'array:');
+    for (let i=0;i<array.length;i++){
+        console.log(array[i]);
+    }
+    console.log(i);
+})();
